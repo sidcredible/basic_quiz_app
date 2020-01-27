2e5071fe434ff1626ad50fc7b5c7712852ccc984
Siddhartha Anand <imsidanand@gmail.com>
2020-01-28 00:27:27 +0530

    used annonymous function

--- lib/main.dart
+++ lib/main.dart
@@ -7,39 +7,43 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  void answerQuestion(){
+  void answerQuestion() {
     print('Answer chosen');
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              'Quiz App',
-            ),
+      appBar: AppBar(
+        title: Text(
+          'Quiz App',
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text('What is the answer of this question ?'),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestion,
           ),
-          body: Column(
-            children: <Widget>[
-              Text('The question'),
-              RaisedButton(
-                child: Text('Answer 1'),
-                onPressed: answerQuestion,
-              ),
-              RaisedButton(
-                child: Text('Answer 2'),
-                onPressed: answerQuestion,
-              ),
-              RaisedButton(
-                child: Text('Answer 3'),
-                onPressed: answerQuestion,
-              ),
-              RaisedButton(
-                child: Text('Answer 4'),
-                onPressed: answerQuestion,
-              )
-            ],
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: answerQuestion,
           ),
-        ));
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: () => print('Answer 2 Chosen'),
+          ),
+          RaisedButton(
+            child: Text('Answer 4'),
+            onPressed: () {
+              //kuch aur bhi
+              print('Answer 4 Chosen');
+            },
+          )
+        ],
+      ),
+    ));
   }
 }
