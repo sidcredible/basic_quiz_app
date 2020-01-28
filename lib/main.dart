bb6b2c3d3bcb503af2e6df3876c9b6d0b96e196f
Siddhartha Anand <imsidanand@gmail.com>
2020-01-28 23:32:08 +0530

    used private class

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,18 @@ class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -41,15 +41,15 @@ class MyAppState extends State<MyApp> {
       body: Column(
         children: <Widget>[
           Text(
-            questions[questionIndex],
+            questions[_questionIndex],
           ),
           RaisedButton(
             child: Text('Answer 1'),
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
           ),
           RaisedButton(
             child: Text('Answer 2'),
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
           ),
           RaisedButton(
             child: Text('Answer 3'),
