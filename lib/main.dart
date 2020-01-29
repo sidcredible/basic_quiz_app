c07412a2a4a20eefc7072f8c888e6d1947f3ac80
Siddhartha Anand <imsidanand@gmail.com>
2020-01-30 01:23:07 +0530

    fixed error after question finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import './answer.dart';
 import './question.dart';
 
 //void main() {
@@ -17,6 +18,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favourite color ?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favourite animal ?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'What\'s your favourite instructor ?',
+      'answers': ['Max', 'Max', 'Max', 'Max'],
+    },
+  ];
+
   var _questionIndex = 0;
 
   void _answerQuestion() {
@@ -24,44 +40,34 @@ class _MyAppState extends State<MyApp> {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print('We have more questions!');
+    }
+    else {
+      print('No more questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favourite color ?',
-      'What\'s your favourite animal ?',
-      'What\'s your college name ?'
-    ];
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Quiz App',
-        ),
-      ),
-      body: Column(
-        children: <Widget>[
-          Question(
-            questions[_questionIndex],
+          appBar: AppBar(
+            title: Text(
+              'Quiz App',
+            ),
           ),
-          RaisedButton(
-            child: Text('Answer 2'),
-            onPressed: _answerQuestion,
-          ),
-          RaisedButton(
-            child: Text('Answer 3'),
-            onPressed: () => print('Answer 2 Chosen'),
-          ),
-          RaisedButton(
-            child: Text('Answer 4'),
-            onPressed: () {
-              //kuch aur bhi
-              print('Answer 4 Chosen');
-            },
-          )
-        ],
-      ),
-    ));
+          body: _questionIndex < questions.length ? Column(
+            children: <Widget>[
+              Question(
+                questions[_questionIndex]['questionText'],
+              ),
+              ...(questions[_questionIndex]['answers'] as List<String>)
+                  .map((answer) {
+                return Answer(_answerQuestion, answer);
+              }).toList()
+            ],
+          ) : Center(child: Text('You did it!'),),
+        ));
   }
 }
